06c9c2f345cbcc97074aea2e9cffb212a7aa6987
Thomas G Wolf <tgreyw@gmail.com>
2022-03-30 20:44:09 -0600

    Removed sudo perm check in docker-ip.sh

--- docker-ip.sh
+++ docker-ip.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ "$EUID" != 0 ]]; then
-  echo -e "\e[91mMust be run using sudo!\e[39m" >&2
-  exit 1
-fi
-
 while read -r container_id; do 
   container_ip="$(docker inspect \
       --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' \
